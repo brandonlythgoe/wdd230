c02b9586d7b135ae51ba9dd582b9fa38ef4d8f0b
brandonlythgoe <113808796+brandonlythgoe@users.noreply.github.com>
2023-02-02 12:30:06 -0700

    major updates

--- Chamber/scripts/header-footer.js
+++ Chamber/scripts/header-footer.js
@@ -3,9 +3,9 @@
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format( new Date() );
 document.querySelector(".header-today p").textContent = fulldate;
 
-// This sets the year for the footer
+//footer
 const today = new Date();
 document.querySelector("#currentyear").textContent = today.getFullYear();
 
-// This sets the last modified date on the home page
+
 document.querySelector("#lastmodified").textContent = document.lastModified;
